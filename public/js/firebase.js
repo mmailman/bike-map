644501f66860c49d92eb0b8d28b726027906f8d3
tetsaiga86 <tetsaiga86@gmail.com>
2016-05-29 21:40:54 -0700

    button console log

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,1 +1,11 @@
 var Firebase = require('firebase');
+$('#data').on('click',function(e){
+  e.preventDefault();
+  var ref = new Firebase("https://bike-map-fd305.firebaseio.com/");
+// Attach an asynchronous callback to read the data at our posts reference
+  ref.on('value', function(snapshot) {
+    console.log(snapshot.val());
+  }, function (errorObject) {
+    console.log('The read failed: ' + errorObject.code);
+  });
+});
