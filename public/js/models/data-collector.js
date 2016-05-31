122fbc322f06c1b8daa4281f78b117f3b4764d2d
tetsaiga86 <tetsaiga86@gmail.com>
2016-05-31 14:53:45 -0700

    fix data

--- public/js/models/data-collector.js
+++ public/js/models/data-collector.js
@@ -3,6 +3,16 @@ var ref = new Firebase('https://bike-map-fd305.firebaseio.com/');
 var gba = [];
 var gda = [];
 
+function gbaAverageFilter(arr,interval){
+  var result = [];
+  for(var i = 0; i < arr.length;i += interval){
+    arr.slice(i, interval).reduce(function(value){
+      return value;
+    });
+    result.push(sum / interval);
+  }
+  return result;
+}
 ref.on('value', function(snapshot) {
   snapshotVal = snapshot.val();
   console.log(snapshot.val());
@@ -14,7 +24,7 @@ ref.on('value', function(snapshot) {
 
 function bikesAvailableAll (){
   gba = [];
-  for(i = 0 ; i < Object.keys(snapshotVal.data).length; i++){
+  for(var i = 0 ; i < Object.keys(snapshotVal.data).length; i++){
     temp = [];
     snapshotVal.data[Object.keys(snapshotVal.data)[i]].stations.forEach(function(station){
       temp.push(station.ba);
@@ -25,7 +35,7 @@ function bikesAvailableAll (){
 
 function docksAvailableAll (){
   gda = [];
-  for(i = 0 ; i < Object.keys(snapshotVal.data).length; i++){
+  for(var i = 0 ; i < Object.keys(snapshotVal.data).length; i++){
     temp = [];
     snapshotVal.data[Object.keys(snapshotVal.data)[i]].stations.forEach(function(station){
       temp.push(station.da);
