5cb43227c9f451f0eb89aa7ad79adf1abe7a203f
tetsaiga86 <tetsaiga86@gmail.com>
2016-05-31 12:43:23 -0700

    merge

--- public/js/models/data-collector.js
+++ public/js/models/data-collector.js
@@ -4,11 +4,12 @@ var ref = new Firebase('https://bike-map-fd305.firebaseio.com/');
 ref.on('value', function(snapshot) {
   snapshotVal = snapshot.val();
   console.log(snapshot.val());
-  snapshotVal.data[1464579162191].stations.forEach(function(station){
-    $('.data').append('<li>' + station.s + '</li>');
-  });
 }, function (errorObject) {
   console.log('The read failed: ' + errorObject.code);
 });
 
-for(i = 0 ; i < Object.keys(snapshotVal.data).length; i++){}
+for(i = 0 ; i < Object.keys(snapshotVal.data).length; i++){
+  snapshotVal.data[i].stations.forEach(function(station){
+    console.log(Object.keys(snapshotVal.data));
+  });
+}
