64afabb1864c67461b5977c1b8bf4bad7629fd99
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-05-31 14:12:39 -0700

    fixing merge conflict from get-data

---
+++ public/js/models/data-collector.js
@@ -0,0 +1,33 @@
+var snapshotVal;
+var ref = new Firebase('https://bike-map-fd305.firebaseio.com/');
+gba = [];
+gda = [];
+
+ref.on('value', function(snapshot) {
+  snapshotVal = snapshot.val();
+  console.log(snapshot.val());
+  bikesAvailableAll();
+  docksAvailableAll();
+}, function (errorObject) {
+  console.log('The read failed: ' + errorObject.code);
+});
+
+function bikesAvailableAll (){
+  for(i = 0 ; i < Object.keys(snapshotVal.data).length; i++){
+    temp = [];
+    snapshotVal.data[Object.keys(snapshotVal.data)[i]].stations.forEach(function(station){
+      temp.push(station.ba);
+      gba.push(temp);
+    });
+  };
+}
+
+function docksAvailableAll (){
+  for(i = 0 ; i < Object.keys(snapshotVal.data).length; i++){
+    temp = [];
+    snapshotVal.data[Object.keys(snapshotVal.data)[i]].stations.forEach(function(station){
+      temp.push(station.ba);
+      gda.push(temp);
+    });
+  };
+}
--- public/js/models/firebase.js
+++ public/js/models/firebase.js
@@ -1,19 +1,3 @@
-var snapshotVal;
-
-$('#data').on('click',function(e){
-  e.preventDefault();
-  console.log('button fucking clicked and I updated');
-  var ref = new Firebase('https://bike-map-fd305.firebaseio.com/');
-  ref.on('value', function(snapshot) {
-    snapshotVal = snapshot.val();
-    console.log(snapshot.val());
-    snapshotVal.data[1464579162191].stations.forEach(function(station){
-      $('.data').append('<li>' + station.s + '</li>');
-    });
-  }, function (errorObject) {
-    console.log('The read failed: ' + errorObject.code);
-  });
-});
 
 // Initialize Firebase
 var config = {
