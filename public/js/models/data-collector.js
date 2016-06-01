1798923b47b434031b1f016c6e53f2587f3fad1c
tetsaiga86 <tetsaiga86@gmail.com>
2016-05-31 17:20:05 -0700

    fix data

--- public/js/models/data-collector.js
+++ public/js/models/data-collector.js
@@ -2,15 +2,27 @@ var snapshotVal;
 var ref = new Firebase('https://bike-map-fd305.firebaseio.com/');
 var gba = [];
 var gda = [];
+var average = [];
 
-var averageFilter = function (array, interval) {
-  var average = [];
+var averageFilter = function (array) {
+  average = [];
   for(var i = 0; i < array.length; i ++) {
     average.push(array[i].reduce(function(prev,cur){
       return (prev + cur);
     }) / array[i].length);
   }
   return average;
+};
+
+var condenseAverage = function(array, interval){
+  var result = [];
+  for (var i = 0; i < array.length; i += interval) {
+    var sum = array.slice(i, i + interval).reduce(function(prev, cur){
+      return prev + cur;
+    });
+    result.push(sum / interval);
+  }
+  return result;
 };
 
 ref.on('value', function(snapshot) {
