d01a5ff8dd38d689d36d258770f970cacdbace91
tetsaiga86 <tetsaiga86@gmail.com>
2016-06-02 14:22:35 -0700

    bug fix

--- public/js/controllers/chartController.js
+++ public/js/controllers/chartController.js
@@ -1,5 +1,5 @@
 $('#station-select').on('change', function(){
-  if($('#station-select').val() == null){
+  if($('#station-select').val() == ''){
     displayAvailChart(condenseAverage(averageAllFilter(gba),12),condenseAverage(averageAllFilter(gda),12));
   }else{
     displayAvailChart(oneStationHourlyAverage(gba,$('#station-select').val()),oneStationHourlyAverage(gda,$('#station-select').val()));
