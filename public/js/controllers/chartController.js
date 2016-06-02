40cf8cd78c801131859c69050130fa7db831d30d
tetsaiga86 <tetsaiga86@gmail.com>
2016-06-02 11:29:45 -0700

    mutate array

--- public/js/controllers/chartController.js
+++ public/js/controllers/chartController.js
@@ -1,4 +1,3 @@
 $('#station-select').on('change', function(){
-  displayAvailChart(oneStationHourlyAverage(gba,$('#station-select').val() - 1),oneStationHourlyAverage(gda,$('#station-select').val() - 1));
-// -1 on .val() to get index number from id
+  displayAvailChart(oneStationHourlyAverage(gba,$('#station-select').val()),oneStationHourlyAverage(gda,$('#station-select').val()));
 });
