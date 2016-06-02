b9d3f61d482f65aad07eb2318b82179c46a9e6c4
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-06-02 13:57:56 -0700

    removed console log messages

--- public/js/controllers/mapController.js
+++ public/js/controllers/mapController.js
@@ -13,12 +13,10 @@
     $('#find-nearest-station').on('click', function(e) {
       e.preventDefault();
       if ($('#find-nearest-station').text() === 'Station Nearest to Me!') {
-        console.log('finding nearest station');
         BikeMap.getDirections();
         $('#find-nearest-station').text('Reset Directions');
       } else {
         BikeMap.resetDirections();
-        console.log('resetting station directions');
         $('#find-nearest-station').text('Station Nearest to Me!');
       }
     });
