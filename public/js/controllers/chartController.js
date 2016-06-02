aaab3b52f71f5ed3954bb1cad72ff05adabf2ee9
tetsaiga86 <tetsaiga86@gmail.com>
2016-06-02 13:51:50 -0700

    pull request , but not done

--- public/js/controllers/chartController.js
+++ public/js/controllers/chartController.js
@@ -1,3 +1,4 @@
 $('#station-select').on('change', function(){
+  if(true){};
   displayAvailChart(oneStationHourlyAverage(gba,$('#station-select').val()),oneStationHourlyAverage(gda,$('#station-select').val()));
 });
